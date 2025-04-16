19e6b8af2804d05b7853f19bc709907aef06e3c6
Royal Northen <royal.northen@tcu.edu>
2025-04-16 10:26:15 -0500

    fixed HW6 AGAIN

--- homework 6/clever_imoprtation_Royal.py
+++ homework 6/clever_imoprtation_Royal.py
@@ -1,11 +1,12 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from NdimGradDescentRoyal import N_Dim_Grad_Descent_Royal
 
 def line(x_in, m, b):
     return m*np.sin(x_in) + b
 
 def line_1(x):
-    return line(x, 500, 0.1)
+    return line(x, 50, 0.1)
 
 def line_2(x):
     return line(x, 10, -4)
@@ -20,3 +21,23 @@ minimum_3, steps_3, path_taken_3 = N_Dim_Grad_Descent_Royal.Ndim_grad_desc(line_
 print(f"minimum of line_1 found to be {minimum_1} in {steps_1} steps.")
 print(f"minimum of line_2 found to be {minimum_2} in {steps_2} steps.")
 print(f"minimum of line_3 found to be {minimum_3} in {steps_3} steps.")
+
+fig1, ax1 = plt.subplots()
+fig2, ax2 = plt.subplots()
+fig3, ax3 = plt.subplots()
+
+ax1.set(xlim=[0, 20], ylim=[-55,55], xlabel='x', ylabel='y', title="minimum of line_1 at the star")
+ax2.set(xlim=[0, 20], ylim=[-20,20], xlabel='x', ylabel='y', title="minimum of line_2 at the star")
+ax3.set(xlim=[0, 20], ylim=[0,10], xlabel='x', ylabel='y', title="minimum of line_3 at the star")
+
+x_array = np.linspace(0, 20, 200)
+
+ax1.plot(x_array, line_1(x_array))
+ax2.plot(x_array, line_2(x_array))
+ax3.plot(x_array, line_3(x_array))
+
+ax1.scatter(minimum_1, line_1(minimum_1), marker='*')
+ax2.scatter(minimum_2, line_2(minimum_2), marker='*')
+ax3.scatter(minimum_3, line_3(minimum_3), marker='*')
+
+plt.show()
